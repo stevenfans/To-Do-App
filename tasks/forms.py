9564a00fd2ca10c606875c9cdf379100e50174a0
Steven Phan <steven.phan009@gmail.com>
2021-08-16 21:02:49 -0700

    finished primary css

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,6 +4,8 @@ from tasks.models import Task
 
 class taskForm(forms.ModelForm):
 
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add new task...'}))
+
     class Meta: 
         model = Task
         fields = '__all__'
